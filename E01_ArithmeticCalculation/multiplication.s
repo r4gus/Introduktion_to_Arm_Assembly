9101193451e30326929f46bfaf8b94a4874c5a60
David Sugar <foobarc11@protonmail.ch>
2019-11-29 00:25:40 +0100

    Exercise 1 created

--- E01_ArithmeticCalculation/multiplication.s
+++ E01_ArithmeticCalculation/multiplication.s
@@ -1,20 +1,20 @@
-;EN: Calculate x = 5 * 7 + 2 without using a multiplication command.
-;DE: Berechnen Sie x = 5 * 7 + 2 ohne dabei einen Multiplikationsbefehl zu verwenden.
+@EN: Calculate x = 5 * 7 + 2 without using a multiplication command.
+@DE: Berechnen Sie x = 5 * 7 + 2 ohne dabei einen Multiplikationsbefehl zu verwenden.
 
 .section .text
 .global _start
 
 _start:
-    ; here begins your code
-    ; Instruktionen hier einfügen
+    @ here begins your code
+    @ Instruktionen hier einfügen
 
 
 
-; Don't think about this code for now
-; Fürs erste ignorieren
-mov r7, #1      ; exit (syscall number)
-mov r0, #0      ; exit success
-svc #0          ; supervisor call (systemcall)
+@ Don't think about this code for now
+@ Fürs erste ignorieren
+mov r7, #1      @ exit (syscall number)
+mov r0, #0      @ exit success
+svc #0          @ supervisor call (systemcall)
 
 
     
